0ad531dd4a3b54d1a35829ec34b1ef15d37c45c0
Sara Segel <sara.segel@gmail.com>
2020-10-01 02:47:19 +0000

    change mongodb connect info in app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var express     = require("express"),
     session = require("express-session"),
     seedDB      = require("./seeds"),
     methodOverride = require("method-override");
+
 // configure dotenv
 require('dotenv').load();
 
@@ -24,14 +25,26 @@ var commentRoutes    = require("./routes/comments"),
 mongoose.Promise = global.Promise;
 
 
-const databaseUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/yelp_meme/v3';
+// const databaseUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/yelp_meme/v3';
 
-mongoose.connect('mongodb://localhost:27017/yelp_meme', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to DB!'))
-.catch(error => console.log(error.message));
+// mongoose.connect('mongodb://localhost:27017/yelp_meme', {no
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+// .then(() => console.log('Connected to DB!'))
+// .catch(error => console.log(error.message));
+
+// mongoose.connect("mongodb+srv://sarasegel:NY%253z%26EN%402k%23S5T1@yelpcamp.owjlv.mongodb.net/test?retryWrites=true"); 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://sarasegel:NY%253z%26EN%402k%23S5T1@yelpcamp.owjlv.mongodb.net/yelpcamp?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+	console.log("MongoDB connected!");
+  client.close();
+});
 
 // var databaseUri = 'mongodb://localhost:27017/yelp_meme/v3';
 // mongoose.connect(databaseUri, { useMongoClient: true })
@@ -44,7 +57,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
 app.use(cookieParser('secret'));
-//require moment
+// require moment
 app.locals.moment = require('moment');
 // seedDB(); //seed the database
 
