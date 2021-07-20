a1f179be96b456c5c955e80f22289d3069a5bc02
Ikarie <Ikarie@ikarie.local>
2021-07-20 23:40:51 +0700

    update dependencies and db connection

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use("/", indexRoutes);
 app.use("/snaps", campgroundRoutes);
 app.use("/snaps/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT || 3000, process.env.IP, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("The Bangkok ♥ Streetheart Server Has Started!");
 });
 
