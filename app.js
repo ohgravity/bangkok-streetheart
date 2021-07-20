ce711a68fb19dd206c02aad77baf69b78addd3bd
Ikarie <Ikarie@ikarie.local>
2021-07-20 20:47:14 +0700

    change mongoose connection to env variable

--- app.js
+++ app.js
@@ -1,57 +1,60 @@
 // configure dotenv
-require('dotenv').load();
+require("dotenv").load();
 
-
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    passport    = require("passport"),
-    cookieParser = require("cookie-parser"),
-    LocalStrategy = require("passport-local"),
-    flash        = require("connect-flash"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    session = require("express-session"),
-    seedDB      = require("./seeds"),
-    methodOverride = require("method-override");
-
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  cookieParser = require("cookie-parser"),
+  LocalStrategy = require("passport-local"),
+  flash = require("connect-flash"),
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  session = require("express-session"),
+  seedDB = require("./seeds"),
+  methodOverride = require("method-override");
 
 // requiring routes
-var commentRoutes    = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index")
-    
+var commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
+
 // assign mongoose promise library and connect to database
 // mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb+srv://sarasegel:NY%253z%26EN%402k%23S5T1@yelpcamp.owjlv.mongodb.net/yelpcamp?retryWrites=true", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-}).then(() => {
-  console.log("Connected to DB!");
-}).catch(err => {
-  console.log("ERROR", err.message);
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch((err) => {
+    console.log("ERROR", err.message);
+  });
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(methodOverride('_method'));
-app.use(cookieParser('secret'));
+app.use(methodOverride("_method"));
+app.use(cookieParser("secret"));
 // require moment
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
 app.use(flash());
 app.use(passport.initialize());
@@ -60,17 +63,17 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.success = req.flash('success');
-   res.locals.error = req.flash('error');
-   next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 app.use("/", indexRoutes);
 app.use("/snaps", campgroundRoutes);
 app.use("/snaps/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT || 3000, process.env.IP, function() {
-	console.log('The Bangkok ♥ Streetheart Server Has Started!');
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
+  console.log("The Bangkok ♥ Streetheart Server Has Started!");
 });
